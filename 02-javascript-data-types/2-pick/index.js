d9a358354182c05dd47392ec6604ed7c4db6ac51
k1ntsugi1 <bmasalimov5@gmail.com>
2022-09-09 00:20:23 +0300

    funcs_of_second_step

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -4,6 +4,27 @@
  * @param {...string} fields - the properties paths to pick
  * @returns {object} - returns the new object
  */
-export const pick = (obj, ...fields) => {
+export const pick = (startObj, ...fields) => {
 
+    // <------------То что закомментировано работает, тесты обрабатываются за ~8sec-------------->
+    // const objOfFields  = fields.reduce((acc, item) => {
+    //     acc[item] = item;
+    //     return acc;
+    // }, {});
+
+    // const getPickedObj = (currentObj) => {
+    //     const reducedCurrentObj = Object.entries(currentObj).reduce((acc, [key, value]) => {
+    //         if (objOfFields[key]) {
+    //             acc[key] = typeof value === 'object' ? getPickedObj(value) : value;
+    //         }
+    //         return acc;
+    //     }, {});
+    //     return reducedCurrentObj;
+    // };
+    // return getPickedObj(startObj);
+
+    // <---Не знаю какое значение BigO ниже, но код гораздо короче и понятнее, а также тесты обрабатываются за ~5sec------>
+
+    const jsonWithPickedFields = JSON.stringify(startObj, fields);
+    return JSON.parse(jsonWithPickedFields);
 };
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -4,6 +4,25 @@
  * @param {...string} fields - the properties paths to omit
  * @returns {object} - returns the new object
  */
-export const omit = (obj, ...fields) => {
+export const omit = (startObj, ...fields) => {
+    const objOfFields  = fields.reduce((acc, item) => {
+        acc[item] = item;
+        return acc;
+    }, {});
+    // <------------То что закомментировано работает, тесты обрабатываются за ~8sec-------------->
+    // const getOmittedObj = (currentObj) => {
+    //     const reducedCurrentObj = Object.entries(currentObj).reduce((acc, [key, value]) => {
+    //         if (!objOfFields[key]) {
+    //             acc[key] = typeof value === 'object' ? getOmittedObj(value) : value;
+    //         }
+    //         return acc;
+    //     }, {});
+    //     return reducedCurrentObj;
+    // };
+    // return getOmittedObj(startObj);
 
+     // <---Не знаю какое значение BigO ниже, но код гораздо короче и понятнее, а также тесты обрабатываются за ~5sec------>
+
+    const jsonWithoutPickedFields = JSON.stringify(startObj, (key, value) => objOfFields[key] ?  undefined : value);
+    return JSON.parse(jsonWithoutPickedFields)
 };
