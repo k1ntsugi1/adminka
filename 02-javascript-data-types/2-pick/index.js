99feddb4395002501a7e4c18f137a87465f6e8d0
k1ntsugi1 <bmasalimov5@gmail.com>
2022-09-10 00:35:14 +0300

    removed JSON.stringify/parse

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,26 +5,20 @@
  * @returns {object} - returns the new object
  */
 export const pick = (startObj, ...fields) => {
+    const objOfFields  = fields.reduce((acc, item) => {
+        acc[item] = item;
+        return acc;
+    }, {});
 
-    // <------------То что закомментировано работает, тесты обрабатываются за ~8sec-------------->
-    // const objOfFields  = fields.reduce((acc, item) => {
-    //     acc[item] = item;
-    //     return acc;
-    // }, {});
+    const getPickedObj = (currentObj) => {
+        const reducedCurrentObj = Object.entries(currentObj).reduce((acc, [key, value]) => {
+            if (objOfFields[key]) {
+                acc[key] = typeof value === 'object' ? getPickedObj(value) : value;
+            }
+            return acc;
+        }, {});
+        return reducedCurrentObj;
+    };
+    return getPickedObj(startObj);
 
-    // const getPickedObj = (currentObj) => {
-    //     const reducedCurrentObj = Object.entries(currentObj).reduce((acc, [key, value]) => {
-    //         if (objOfFields[key]) {
-    //             acc[key] = typeof value === 'object' ? getPickedObj(value) : value;
-    //         }
-    //         return acc;
-    //     }, {});
-    //     return reducedCurrentObj;
-    // };
-    // return getPickedObj(startObj);
-
-    // <---Не знаю какое значение BigO ниже, но код гораздо короче и понятнее, а также тесты обрабатываются за ~5sec------>
-
-    const jsonWithPickedFields = JSON.stringify(startObj, fields);
-    return JSON.parse(jsonWithPickedFields);
 };
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -9,20 +9,15 @@ export const omit = (startObj, ...fields) => {
         acc[item] = item;
         return acc;
     }, {});
-    // <------------То что закомментировано работает, тесты обрабатываются за ~8sec-------------->
-    // const getOmittedObj = (currentObj) => {
-    //     const reducedCurrentObj = Object.entries(currentObj).reduce((acc, [key, value]) => {
-    //         if (!objOfFields[key]) {
-    //             acc[key] = typeof value === 'object' ? getOmittedObj(value) : value;
-    //         }
-    //         return acc;
-    //     }, {});
-    //     return reducedCurrentObj;
-    // };
-    // return getOmittedObj(startObj);
+    const getOmittedObj = (currentObj) => {
+        const reducedCurrentObj = Object.entries(currentObj).reduce((acc, [key, value]) => {
+            if (!objOfFields[key]) {
+                acc[key] = typeof value === 'object' ? getOmittedObj(value) : value;
+            }
+            return acc;
+        }, {});
+        return reducedCurrentObj;
+    };
+    return getOmittedObj(startObj);
 
-     // <---Не знаю какое значение BigO ниже, но код гораздо короче и понятнее, а также тесты обрабатываются за ~5sec------>
-
-    const jsonWithoutPickedFields = JSON.stringify(startObj, (key, value) => objOfFields[key] ?  undefined : value);
-    return JSON.parse(jsonWithoutPickedFields)
 };
