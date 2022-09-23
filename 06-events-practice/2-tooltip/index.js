6bf069242b8ac3a3b9828e87e973836c5389827b
k1ntsugi1 <bmasalimov5@gmail.com>
2022-09-23 21:53:04 +0300

    +06-tooltip, all are working | tests doesnt thind so

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,88 @@
 class Tooltip {
-  initialize () {
+  constructor() {
+    if (!Tooltip.instance) {
+      Tooltip.instance = this;
+    } 
+    return Tooltip.instance;
+  }
 
+  render(dataOfTooltip = '') {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = `<div class="tooltip" hidden>${dataOfTooltip}</div>`;
+
+    this.element = wrapper.firstElementChild;
+    document.body.append(this.element);
+  }
+
+  showTooltip(newDataOfTooltip) {
+    this.dataOftooltip = newDataOfTooltip;
+
+    this.element.hidden = false;
+    this.element.textContent = this.dataOftooltip;
+    //document.body.append(this.element);
+  }
+
+  hideTooltip() {
+    this.element.hidden = true;
+    this.dataOftooltip = null;
+    //document.body.removeChild(this.element);
+  }
+
+  destroy() {
+    this.removeTooltipInDocument();
+    this.removeEventListeners();
+    this.element?.remove();
+  }
+
+  initialize () {
+    this.render();
+    this.addPointerOutListener();
+  }
+
+  pointerOutHandler = (event) => {
+    const tooltipTarget = event.relatedTarget?.closest('[data-tooltip]');
+    if (!tooltipTarget) {
+      this.hideTooltip();
+      this.removePointerMoveListener();
+      return;
+    }
+
+    const { dataset: 
+      {
+        tooltip: newDataOfTooltip = null,
+      },
+    } = tooltipTarget;
+
+    if (this.dataOftooltip === newDataOfTooltip) {return;}
+
+    this.showTooltip(newDataOfTooltip);
+    this.addPointerMoveListener();
+  }
+
+  pointerMoveHandler = (event) => {
+    const { clientX, clientY } = event;
+    this.element.style.left = clientX + 10 + 'px';
+    this.element.style.top = clientY + 15 + 'px';
+  }
+
+  addPointerMoveListener() {
+    document.addEventListener('pointermove', this.pointerMoveHandler);
+  }
+
+  removePointerMoveListener() {
+    document.removeEventListener('pointermove', this.pointerMoveHandler);
+  }
+
+  addPointerOutListener() {
+    document.addEventListener('pointerout', this.pointerOutHandler);
+  }
+
+  removePointerOutListener() {
+    document.removeEventListener('pointerout', this.pointerOutHandler);
+  }
+  removeEventListeners() {
+    this.removePointerOutListener();
+    this.removePointerMoveListener();
   }
 }
 
